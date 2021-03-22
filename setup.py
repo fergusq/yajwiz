1597077a7709d66167faa44d1f728e31284a4581
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2021-03-23 00:51:57 +0200

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yajwiz",
-    version="0.3.0",
+    version="0.4.0",
     author="Iikka Hauhio",
     author_email="fergusq@kaivos.org",
     packages=["yajwiz"],
